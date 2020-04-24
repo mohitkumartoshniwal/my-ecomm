a8b312c9a2690bdd5c185ba9e56d48e21364f045
mohitkumartoshniwal <mohitkrtoshniwal.com>
2020-04-24 18:22:09 +0530

    configured router for hats componenet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.componenet';
+import {Route , Switch } from 'react-router-dom';
+
+
+const HatsPage=() =>(
+  <div>Hats PAge</div>
+)
+ 
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* Switch used for rendering only one route when matched */}
+      <Switch> 
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/hats' component={HatsPage}/>
+      </Switch>
+     
     </div>
   );
 }
